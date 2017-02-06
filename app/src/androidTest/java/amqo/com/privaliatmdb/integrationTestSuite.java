695d395fdfb7f0d445aa52f4b398ee97679ffef2
Alberto Muñoz <alberto.mqo@gmail.com>
2017-02-06 18:16:27 +0100

    Remove BaseActivityTest from suite.

--- app/src/androidTest/java/amqo/com/privaliatmdb/integrationTestSuite.java
+++ app/src/androidTest/java/amqo/com/privaliatmdb/integrationTestSuite.java
@@ -5,7 +5,6 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        BaseActivityTest.class,
         MainActivityTest.class,
         SearchActivityTest.class
 })
